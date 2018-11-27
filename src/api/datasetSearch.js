ffd15b37652af9894b037386b8665a55b13cc759
Morten Høfft <mhoefft@gbif.org>
2018-11-27 13:46:03 +0100

    use UAT endpoints for search

--- src/api/datasetSearch.js
+++ src/api/datasetSearch.js
@@ -3,67 +3,67 @@ import axios from 'axios'
 import setHeaders from './setHeaders'
 
 export const searchDatasets = function(query) {
-  return fetch(`http://api.gbif.org/v1/dataset?${qs.stringify(query)}`, {
+  return fetch(`//api.gbif-uat.org/v1/dataset?${qs.stringify(query)}`, {
     headers: setHeaders()
   })
 };
 
 export const searchDeletedDatasets = function(query) {
-  return fetch(`http://api.gbif.org/v1/dataset/deleted?${qs.stringify(query)}`, {
+  return fetch(`//api.gbif-uat.org/v1/dataset/deleted?${qs.stringify(query)}`, {
     headers: setHeaders()
   })
 };
 
 export const getDataset = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetContacts = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/contact`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/contact`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetIdentifier = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/identifier`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/identifier`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetEndpoint = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/endpoint`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/endpoint`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetTags = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/tag`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/tag`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetMachineTags = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/machineTag`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/machineTag`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetComment = function(key) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/comment`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/comment`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetConstituents = function(key, query) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/constituents${qs.stringify(query)}`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/constituents${qs.stringify(query)}`, {
     headers: setHeaders()
   })
 };
 
 export const getDatasetProcess = function(key, query) {
-  return axios.get(`http://api.gbif.org/v1/dataset/${key}/process${qs.stringify(query)}`, {
+  return axios.get(`//api.gbif-uat.org/v1/dataset/${key}/process${qs.stringify(query)}`, {
     headers: setHeaders()
   })
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
 
 const Home = () => {
-  const name = 'Benny'
-  const unreadCount = 21
   return (
     <div style={{ padding: 24, background: 'white' }}>
       <p>
-        <FormattedMessage
-          id="welcome"
-          defaultMessage={`Hej {name}, you have {unreadCount, number} {unreadCount, plural,
-          one {message}
-          other {messages}
-        }`}
-          values={{ name: <b>{name}</b>, unreadCount }}
-        />
+        What should the homepage show. Perhaps as the current links to search types. We could add recently visited resources.
+      </p>
+      <p>
+        If logged in it could also show a dashboard: missing endorsements. failing dataset crawls.
       </p>
 
-      <p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p><p>sdfjh</p><p>aa</p>
     </div>
   )
 }
