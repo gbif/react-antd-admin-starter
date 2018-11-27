6740f08243d0c24c256ca9999337afd1533dc077
Morten Høfft <mhoefft@gbif.org>
2018-11-27 16:58:04 +0100

    make data table a func component

--- src/components/Dataset/DataTable.js
+++ src/components/Dataset/DataTable.js
@@ -4,48 +4,46 @@ import { Input, Table } from 'antd';
 
 const Search = Input.Search;
 
-class DataTable extends React.Component {
-  render() {
-    const { searchable, updateQuery, fetchData, data, query, loading, columns } = this.props;
-    const { q } = query
-    const Header = <FormattedMessage id="nResults"
-      defaultMessage={`{resultCount, number} {resultCount, plural,
-          one {results}
-          other {results}
+const DataTable = (props) => {
+  const { searchable, updateQuery, fetchData, data, query, loading, columns } = props;
+  const { q } = query
+  const Header = <FormattedMessage id="nResults"
+    defaultMessage={`{resultCount, number} {resultCount, plural,
+        one {results}
+        other {results}
+      }
+    `}
+    values={{ resultCount: data.count, q }}
+  />;
+  return (
+    <React.Fragment>
+      <div style={{ background: 'white', padding: 16 }}>
+        {searchable && <Search
+          placeholder="input search text"
+          enterButton="Search"
+          size="large"
+          onChange={(e) => updateQuery({ q: e.target.value })}
+          value={q}
+          onSearch={val => fetchData({q: val})}
+          style={{ marginBottom: '16px' }}
+        />
         }
-      `}
-      values={{ resultCount: data.count, q }}
-    />;
-    return (
-      <React.Fragment>
-        <div style={{ background: 'white', padding: 16 }}>
-          {searchable && <Search
-            placeholder="input search text"
-            enterButton="Search"
-            size="large"
-            onChange={(e) => updateQuery({ q: e.target.value })}
-            value={q}
-            onSearch={val => fetchData({q: val})}
-            style={{ marginBottom: '16px' }}
-          />
-          }
-          <Table
-            columns={columns}
-            dataSource={data.results}
-            bordered
-            title={() => Header}
-            pagination={{
-              total: data.count,
-              current: 1 + data.offset / data.limit,
-              pageSize: data.limit,
-              onChange: page => fetchData({ q: q, offset: (page - 1) * data.limit })
-            }}
-            loading={loading}
-          />
-        </div>
-      </React.Fragment>
-    )
-  }
+        <Table
+          columns={columns}
+          dataSource={data.results}
+          bordered
+          title={() => Header}
+          pagination={{
+            total: data.count,
+            current: 1 + data.offset / data.limit,
+            pageSize: data.limit,
+            onChange: page => fetchData({ q: q, offset: (page - 1) * data.limit })
+          }}
+          loading={loading}
+        />
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default DataTable
